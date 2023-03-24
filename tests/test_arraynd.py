bfa5af87b59cc3142597b9b29953a273846d3527
Ferenc Vajda <github@fercsi.com>
2023-03-24 21:17:50 +0000

    Enhance ArrayND tests

--- tests/test_arraynd.py
+++ tests/test_arraynd.py
@@ -2,13 +2,8 @@ import pytest
 
 from stretchy import ArrayND
 
-@pytest.mark.parametrize('default',
-    (42, '#', 42.69, None, False)
-)
-def test_default(default):
-    s = ArrayND(3, default)
-    assert s[2,2,2] == default
-    assert s[-2,-2,-2] == default
+def rows_to_str(rows):
+    return '\n'.join(rows)
 
 
 def view(offset, *planes):
@@ -19,6 +14,30 @@ def view(offset, *planes):
         for row in plane:
             v.append(row)
     return '\n'.join(v)
+
+
+@pytest.fixture
+def array():
+    s = ArrayND(dim=4, default='.', offset=-1, content=
+        (((('x', None),
+           ('.', 234)),
+          (('.', False),
+           (6.7, 1.1))),
+         (((None, None),
+           (None, 'y')),
+          ((-1, True),
+           ('', None))))
+    )
+    return s
+
+
+@pytest.mark.parametrize('default',
+    (42, '#', 42.69, None, False)
+)
+def test_default(default):
+    s = ArrayND(3, default)
+    assert s[2,2,2] == default
+    assert s[-2,-2,-2] == default
 
 
 def test_replace_content():
@@ -40,12 +59,128 @@ def test_replace_content():
     assert f'{s:si}' == exp
 
 
-def test_setitem():
-    pass # TODO
+SE2 = [['...']*3] * 2
+SE3 = [['...']*3] * 3
+SP = ['...','...','..#']
+SN = ['#..','...','...']
+@pytest.mark.parametrize('pos, content',
+    (
+        (((0,0,0),), (['#'])),
+        (((2,2,2),), (*SE2, SP)),
+        (((6,2,2),(2,2,2)), (*SE2, SP, *SE3, SP)),
+        (((2,2,2),(6,2,2)), (*SE2, SP, *SE3, SP)),
+        (((-3,-3,-3),), (SN, *SE2)),
+        (((-7,2,2),(-3,2,2)), (SP, *SE3, SP, *SE2)),
+        (((-3,2,2),(-7,2,2)), (SP, *SE3, SP, *SE2)),
+        (((-3,2,2),(2,2,2)), (SP, *SE2, *SE2, SP)),
+        (((3,2,2),(-2,2,2)), (SP, *SE2, *SE2, SP)),
+    )
+)
+def test_setitem(pos, content):
+    dim = len(pos[0])
+    offset = min(p[0] for p in pos)
+    if offset > 0:
+        offset = 0
+    s = ArrayND(dim=dim, default='.')
+    for p in pos:
+        s[p] = '#'
+    assert f'{s:si}' == view(offset, *content)
 
 
-def test_getitem():
-    pass # TODO
+@pytest.mark.parametrize('index, content',
+    (
+        ((-1,-1,-1,-1), 'x'),
+        ((-2,-1,-1,-1), '.'),
+        ((-1,-1,-2,-1), '.'),
+        ((-1,-1,-1,-2), '.'),
+        ((0,0,0,0), None),
+        ((1,0,0,0), '.'),
+        ((0,0,1,0), '.'),
+        ((0,0,0,1), '.'),
+    )
+)
+def test_getitem(index, content, array):
+    assert array[index] == content
+
+
+def test_getitem_plane(array):
+    plane = array[-1]
+    expected = rows_to_str(["x ", ". 234", "", ". False", "6.7 1.1"])
+    assert f'{plane}' == expected
+    plane[-1,-1,-1] = 69
+    expected = rows_to_str([
+            "69 ", ". 234", "", ". False", "6.7 1.1", "", "",
+            " ", " y", "", "-1 True", " "
+        ])
+    assert f'{array}' == expected
+
+
+SLICE_INPUT = (
+    ((None,), '|||||||||', 'abcdefghi'),
+
+    ((-2,None), 'ab|||||||', 'cdefghi'),
+    ((0,None), 'abcd|||||', 'efghi'),
+    ((2,None), 'abcdef|||', 'ghi'),
+    ((-6,None), '|||||||||||', '..abcdefghi'),
+    ((6,None), 'abcdefghi', ''),
+
+    ((None,-2), '||cdefghi', 'ab'),
+    ((None,0), '||||efghi', 'abcd'),
+    ((None,2), '||||||ghi', 'abcdef'),
+    ((None,-6), 'abcdefghi', ''),
+    ((None,7), '|||||||||||', 'abcdefghi..'),
+
+    ((-2,2), 'ab||||ghi', 'cdef'),
+    ((-6,2), '||||||||ghi', '..abcdef'),
+    ((-2,7), 'ab|||||||||', 'cdefghi..'),
+    ((-6,7), '|||||||||||||', '..abcdefghi..'),
+
+    ((None,None,3), '|bc|ef|hi', 'adg'),
+    ((None,None,-3), 'ab|de|gh|', 'ifc'),
+    ((-6,7,3), '|.a|cd|fg|i.|', '.beh.'),
+    ((6,-7,-3), '|.a|cd|fg|i.|', '.heb.'),
+)
+
+@pytest.mark.parametrize('indices, content, got', SLICE_INPUT)
+def test_getitem_slice1d(indices, content, got):
+    s = ArrayND(dim=2, default='.')
+    for i, c in enumerate('abcdefghi', -4):
+        j = round(i/4)
+        s[i,j] = c
+    # check read planes
+    check = []
+    for plane in s[slice(*indices)]:
+        c = max(plane[i] for i in (-1, 0, 1))
+        check.append(c)
+        plane[0] = '|' # write
+    assert ''.join(check) == got
+    # check written planes
+    check = []
+    for plane in s:
+        c = max(plane[i] for i in (-1, 0, 1))
+        check.append(c)
+    assert ''.join(check) == content
+
+
+@pytest.mark.parametrize('indices, content, got', SLICE_INPUT)
+def test_getitem_slicend(indices, content, got):
+    s = ArrayND(dim=3, default='.')
+    for i, c in enumerate('abcdefghi', -4):
+        j = round(i/4)
+        s[i,j,j] = c
+    # check read planes
+    check = []
+    for plane in s[slice(*indices)]:
+        c = max(plane[i,i] for i in (-1, 0, 1))
+        check.append(c)
+        plane[0,0] = '|' # write
+    assert ''.join(check) == got
+    # check written planes
+    check = []
+    for plane in s:
+        c = max(plane[i,i] for i in (-1, 0, 1))
+        check.append(c)
+    assert ''.join(check) == content
 
 
 INPUT_DATA = (
@@ -116,27 +251,86 @@ def test_iter(cells, boundaries, planes):
     assert tuple(f'{sub:s}' for sub in s) == planes
 
 
-@pytest.fixture
-def array():
-    s = ArrayND(dim=3, default='.', offset=-1, content=
-        ((('x', None),
-          ('.', 234)),
-         (('.', False),
-          (6.7, 1.1)))
-    )
-    return s
-
-
 def test_str(array):
-    pass # TODO
+    expected = [
+        '[[[[x          ]',
+        '   [.       234]]',
+        '',
+        '  [[.     False]',
+        '   [  6.7   1.1]]]',
+        '',
+        '',
+        ' [[[           ]',
+        '   [      y    ]]',
+        '',
+        '  [[   -1 True ]',
+        '   [           ]]]]',
+    ]
+    assert str(array) == rows_to_str(expected)
 
 
 def test_repr(array):
-    pass # TODO
+    expected = [
+        "ArrayND(dim=4, default='.', offset=(-1, -1, -1, -1), content=",
+        "[[[['x'  , None ],",
+        "   ['.'  ,   234]],",
+        "",
+        "  [['.'  , False],",
+        "   [  6.7,   1.1]]],",
+        "",
+        "",
+        " [[[None , None ],",
+        "   [None , 'y'  ]],",
+        "",
+        "  [[   -1, True ],",
+        "   [''   , None ]]]])",
+    ]
+    assert repr(array) == rows_to_str(expected)
 
 
-def test_format(array):
-#>    print(f'{array:a}')
-    pass # TODO
+@pytest.mark.parametrize('fmt, expected',
+    (
+        ( "{}", [
+                "x ", ". 234", "", ". False", "6.7 1.1", "", "",
+                " ", " y", "", "-1 True", " "
+            ]),
+        ( "{:s,}", [
+                "x,", ".,234", "", ".,False", "6.7,1.1", "", "",
+                ",", ",y", "", "-1,True", ","
+            ]),
+        ( "{:s,b<e>}", [
+                "<<<<x,>", "   <.,234>>",
+                "", "  <<.,False>", "   <6.7,1.1>>>",
+                "", "", " <<<,>", "   <,y>>",
+                "", "  <<-1,True>", "   <,>>>>"
+            ]),
+        ( "{:s,b<e>r;}", [
+                "<<<<x,>;", "   <.,234>>;",
+                "", "  <<.,False>;", "   <6.7,1.1>>>;",
+                "", "", " <<<,>;", "   <,y>>;",
+                "", "  <<-1,True>;", "   <,>>>>"
+            ]),
+        ( "{:s,b<e>r;a}", [
+                "<<<<x    ,     >;", "   <.    ,  234>>;", "",
+                "  <<.    ,False>;", "   <  6.7,  1.1>>>;", "", "",
+                " <<<     ,     >;", "   <     ,y    >>;", "",
+                "  <<   -1,True >;", "   <     ,     >>>>"
+            ]),
+        ( '{:s,b<e>r;al}', [
+                "<<<<'x'  ,None >;", "   <'.'  ,  234>>;", "",
+                "  <<'.'  ,False>;", "   <  6.7,  1.1>>>;", "", "",
+                " <<<None ,None >;", "   <None ,'y'  >>;", "",
+                "  <<   -1,True >;", "   <''   ,None >>>>"
+            ]),
+        ( '{:s,r;ali}', [
+                "Index -1,-1:", "'x'  ,None ;", "'.'  ,  234;",
+                "Index -1,0:", "'.'  ,False;", "  6.7,  1.1;",
+                "Index 0,-1:", "None ,None ;", "None ,'y'  ;",
+                "Index 0,0:", "   -1,True ;", "''   ,None "
+            ]),
+    )
+)
+def test_format(fmt, expected, array):
+    assert fmt.format(array) == rows_to_str(expected)
 
 
